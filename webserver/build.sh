9064234b0c8f6e1294c44def36a6f7ad94b181be
Kevin Wang <wangkevin448@gmail.com>
2016-09-13 02:44:56 -0700

    production env for webserver

--- webserver/build.sh
+++ webserver/build.sh
@@ -2,6 +2,9 @@
 
 export NODE_ENV=production
 
-npm run build
+if [ ! -d "public" ]; then
+  mkdir public
+fi
 
-cp src/dashboard.html public/dashboard.html && cp src/index.html public/index.html
+
+cp src/dashboard.html public/dashboard.html && cp src/index.html public/index.html && cp node_modules/bullet-tracer/lib/dashboard.js public/dashboard.js && cp node_modules/bullet-tracer/lib/dashboardstyle.css public/dashboardstyle.css && cp node_modules/bullet-tracer/lib/index.js public/index.js && cp node_modules/bullet-tracer/lib/indexstyle.css public/indexstyle.css
--- webserver/src/index.js
+++
@@ -1,8 +0,0 @@
-import {Tracer} from 'bullet-tracer/lib/index';
-
-const app = new Tracer({
-  'basePagesUrl': 'http://localhost:8080/api/pages',
-  'baseConfigUrl': 'http://localhost:8080/api/config'
-});
-
-app.render();
