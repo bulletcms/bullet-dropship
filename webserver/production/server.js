021988ad4feb47047beee785868acaa4e6f2aa4a
Kevin Wang <wangkevin448@gmail.com>
2016-09-13 18:39:31 -0700

    prepare for docker build

--- webserver/production/server.js
+++ webserver/production/server.js
@@ -10,8 +10,6 @@ const sabot = new Sabot(
   {
     log: true,
     servePath: __dirname+'/public',
-    dashboardPath: __dirname+'public/dashboard.html',
-    indexPath: __dirname+'public/index.html',
     staticMaxAge: 16384,
     dynamicMaxAge: 128
   }
