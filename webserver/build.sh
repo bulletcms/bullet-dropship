bb7a3c79e9b98587f18263651e62c6c749ad530c
Kevin Wang <wangkevin448@gmail.com>
2016-09-13 19:53:02 -0700

    local webserver running on docker

--- webserver/build.sh
+++ webserver/build.sh
@@ -6,7 +6,11 @@ if [ ! -d "public" ]; then
   mkdir public
 fi
 
+if [ ! -d "public/dashboard" ]; then
+  mkdir public/dashboard
+fi
 
-cp src/dashboard.html public/dashboard.html && cp src/index.html public/index.html && cp node_modules/bullet-tracer/lib/dashboard.js public/dashboard.js && cp node_modules/bullet-tracer/lib/dashboardstyle.css public/dashboardstyle.css && cp node_modules/bullet-tracer/lib/index.js public/index.js && cp node_modules/bullet-tracer/lib/indexstyle.css public/indexstyle.css
+
+cp src/dashboard.html public/dashboard/index.html && cp src/index.html public/index.html && cp node_modules/bullet-tracer/lib/dashboard.js public/dashboard.js && cp node_modules/bullet-tracer/lib/dashboardstyle.css public/dashboardstyle.css && cp node_modules/bullet-tracer/lib/index.js public/index.js && cp node_modules/bullet-tracer/lib/indexstyle.css public/indexstyle.css
 
 docker build -t bullet-sabot .
